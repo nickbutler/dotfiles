d345fa7783cb898920598d6ad4ab97ce2158ae26
Nick Butler <nick@codeindulgence.com>
2026-05-11 21:57:08 +1000

    Fix lualine chevrons

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,13 @@
 require('lualine').setup({
   options = {
     theme = 'nordfox',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = { 'vim-plug', 'ranger', 'tig' },
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } }, 'diagnostics' },
+    lualine_b = { 'branch', { 'diff', symbols = { added = ' ', modified = ' ', removed = ' ' } }, 'diagnostics' },
     lualine_c = { 'filename' },
     lualine_x = { 'filetype' },
     lualine_y = { 'progress' },
