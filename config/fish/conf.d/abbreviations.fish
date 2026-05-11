5ac0200473a0c8763d4d4e5cde96868ac1f626dd
Nick Butler <nick@codeindulgence.com>
2026-05-11 17:38:43 +1000

    Fish: Added some abbreviations

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -13,12 +13,13 @@ abbr gdi git diff
 abbr gl git log --oneline --decorate --color --graph
 abbr mk mkdir -p
 abbr prb git prb
-abbr push git push
 abbr pull git pull
+abbr push git push
 abbr pw pwgen -Bys 24 1
 abbr r ranger
 abbr rmd rm -rf
 abbr s git status -bsu
+abbr sql lazysql
 abbr t tig --all
 abbr tailf tail -f
 abbr v nvim
